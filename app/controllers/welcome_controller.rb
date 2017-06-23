63bac09b8319f76fa416e45f2a87f9d0306d8848
Arnon Hongklay <arnon@hongklay.com>
2017-06-24 00:01:14 +0700

    render fight

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,18 @@
 class WelcomeController < ApplicationController
   def index
+    token = ENV['AMADEUS_KEY']
+
+    domain = 'http://api.sandbox.amadeus.com/v1.2/flights'
+    origin = 'BOS'
+    departure_date = '2017-09-06--2017-09-26'
+    duration = '7--9'
+    max_price = '500'
+
+    @data = open("#{domain}/inspiration-search?origin=#{origin}&departure_date=#{departure_date}&duration=#{duration}&max_price=#{max_price}&apikey=#{token}").read
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @data }
+    end
   end
 end
