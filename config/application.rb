466f8e43088d76fdf996965b30d646d03423d919
Arnon Hongklay <arnon@hongklay.com>
2017-06-24 18:00:10 +0700

    rails 5.2.0.alpha

--- config/application.rb
+++ config/application.rb
@@ -19,13 +19,13 @@ Bundler.require(*Rails.groups)
 module Fight
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    # config.generators.system_tests = nil
   end
 end
