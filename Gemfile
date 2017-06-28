c2a4acb57ea199e2eac9677df1ae7b523be1f441
Arnon Hongklay <arnon@hongklay.com>
2017-06-28 13:42:24 +0700

    add react to systems

--- Gemfile
+++ Gemfile
@@ -30,3 +30,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'foreman'
+
+gem "webpacker"
+gem "react-rails"
---
+++ app/javascript/components/Hi.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+const Hi = props => <div>Hi {props.name}!</div>;
+
+Hi.defaultProps = {
+  name: "David"
+};
+
+Hi.propTypes = {
+  name: PropTypes.string
+};
+//
+// document.addEventListener("DOMContentLoaded", () => {
+//   ReactDOM.render(
+//     <Hi name="React" />,
+//     document.body.appendChild(document.createElement("div"))
+//   );
+// });
+
+export default Hi;
