a335998ef533e222ebf86eff540b35ac5bb3f73d
Arnon Hongklay <arnon@hongklay.com>
2017-07-02 00:52:44 +0700

    add new react router

--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-// import { Router, Route, hashHistory } from "react-router";
-import { HashRouter, Route, Link } from "react-router-dom";
-
-// const App = () => {
-//   return (
-//     <div className="container">
-//       <Search />
-//     </div>
-//   );
-// };
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import About from "../components/About";
 import Repos from "../components/Repos";
 import Search from "../components/Search";
 
 ReactDOM.render(
-  <HashRouter>
+  <Router>
     <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/repos">Topics</Link>
+        </li>
+      </ul>
+
+      <hr />
+
       <Route exact path="/" component={Search} />
-      <Route path="about" component={About} />
-      <Route path="repos" component={Repos} />
+      <Route path="/about" component={About} />
+      <Route path="/repos" component={Repos} />
     </div>
-  </HashRouter>,
+  </Router>,
   document.getElementById("app")
 );
